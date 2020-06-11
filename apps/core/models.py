fcf805fdcac98a46533d89165451f17261621ec6
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-06-10 22:37:22 -0300

    feat: TimeStamp model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('atualizado em', auto_now=True)
+
+    class Meta:
+        abstract = True
