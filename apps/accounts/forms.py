4c3a060ad84d7978293b4625c9bbb01667f76669
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-06-12 02:03:52 -0300

    feat: form to school

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django import forms
 from .models import *
+from apps.school.models import SchoolInstitution
 from django.contrib.auth import get_user_model
 
 
@@ -9,7 +10,6 @@ class UserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['name', 'username']
-
 
 	def clean_email(self):
 		email = self.cleaned_data.get('email')
@@ -25,3 +25,22 @@ class UserForm(UserCreationForm):
 		return email
 
 
+
+class UserSchoolForm(UserCreationForm):
+
+	class Meta:
+		model = SchoolInstitution
+		fields = ['name', 'username', 'email', 'phone', 'address','cod_student','cod_teacher']
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+
+		# Valido o email para ser obrigatório
+		if email == '':
+			raise forms.ValidationError('O endereço de email é obrigatório.')
+		return email
+
+		if email and User.objects.filter(email=email).count():
+			raise forms.ValidationError(
+				'Este endereço de email já está em uso. Por favor, use um email diferente.')
+		return email
