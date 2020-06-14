ffbf39118142ffa5f1896a9a3486808b96099552
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-06-13 21:32:07 -0300

    feat: view to create user teacher

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, RedirectView
 
-from .forms import UserSchoolForm
+from .forms import UserSchoolForm, UserTeacherlForm
+
 
 class RegisterSchool(CreateView):
     template_name = "school/register.html"
     form_class = UserSchoolForm
     
+
+class RegisterTeacher(CreateView):
+    template_name = "teacher/register.html"
+    form_class = UserSchoolForm
