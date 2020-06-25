691b06bb15201e5039caa5073a1fae67b49bf32e
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-06-24 21:53:44 -0300

    feat: rout to subject view

--- apps/school/urls.py
+++ apps/school/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListTeachersSchool
+from .views import ListTeachersSchool, ListAddSubjects
 
 app_name = 'school'
 
 urlpatterns = [
 
     path('meus-professores/', ListTeachersSchool.as_view(), name='list_teachers'),
+    path('materias/', ListAddSubjects.as_view(), name='subjects'),
 
 ]
