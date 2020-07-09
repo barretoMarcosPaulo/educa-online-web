a3caa9be800ab1eab7a04aca371ef51a310402dc
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-07-09 09:18:59 -0300

    feat: template to subjects teachers

--- apps/school/urls.py
+++ apps/school/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ListTeachersSchool, ListAddSubjects, ListAddClassRoomm
+from .views import ListTeachersSchool, ListAddSubjects, ListAddClassRoomm, TeacherClassRoom
+from .views import TeacherSubjects
 
 app_name = 'school'
 
@@ -8,5 +9,8 @@ urlpatterns = [
     path('meus-professores/', ListTeachersSchool.as_view(), name='list_teachers'),
     path('materias/', ListAddSubjects.as_view(), name='subjects'),
     path('turmas/', ListAddClassRoomm.as_view(), name='class_room'),
+
+    path('professor/materias/', TeacherSubjects.as_view(), name='subjects_teacher'),
+    path('professor/turmas/', TeacherClassRoom.as_view(), name='class_room_teacher'),
 
 ]
